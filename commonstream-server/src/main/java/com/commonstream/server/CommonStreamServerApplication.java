f0c5ee04e7406bb1bf0d8baca4dc91048d971b1f
jwb10028 <143141491+jwb10028@users.noreply.github.com>
2025-09-28 11:10:43 -0400

    scaffold backend server

---
+++ commonstream-server/src/main/java/com/commonstream/server/CommonStreamServerApplication.java
@@ -0,0 +1,13 @@
+package com.commonstream.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonStreamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonStreamServerApplication.class, args);
+	}
+
+}
---
+++ commonstream-server/src/test/java/com/commonstream/server/CommonStreamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commonstream.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonStreamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
