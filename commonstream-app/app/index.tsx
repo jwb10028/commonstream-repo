2d10bfe26906b655590c3aaf64f26b2286d2882e
jwb10028 <143141491+jwb10028@users.noreply.github.com>
2025-10-01 21:20:08 -0400

    hello commonstream

---
+++ commonstream-app/app/index.tsx
@@ -0,0 +1,25 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title" style={styles.text}>
+        Hello Commonstream
+      </ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
